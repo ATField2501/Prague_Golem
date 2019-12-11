e163941729cba77899fa0053f0b715a7b26694ea
ATField2501 <atfield2501@gmail.com>
2019-12-11 12:59:28 +0100

    Modification du script daemon. Respect des normes pour edition de l' en-tête

--- Prague_Golem_Daemon.sh
+++ Prague_Golem_Daemon.sh
@@ -1,9 +1,22 @@
 #!/bin/sh -e
- 
+
+#
+### BEGIN INIT INFO
+# Provides:          ATField2501
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Bot irc
+### END INIT INFO
+
+## author: cagliostro <atfield2501@gmail.com>
+
 DAEMON="/home/cagliostro/Documents/Prague_Golem/Prague_Golem.py" #ligne de commande du programme.
-daemon_OPT="-d -p -f /home/cagliostro/Prague_Golem/Prague_Golem_Daemon.ini"  #argument à utiliser par le programme - Remplacer Utilisateur par votre nom de login
+daemon_OPT="-d -p -f /home/cagliostro/Prague_Golem/Prague_Golem_Daemon.ini"  #argument à utiliser par le programme 
 DAEMONUSER="cagliostro" #utilisateur du programme
 daemon_NAME="Prague_Golem_Daemon.sh" #Nom du programme (doit être identique à l'exécutable).
+
  
 PATH="/sbin:/bin:/usr/sbin:/usr/bin" #Ne pas toucher
  
