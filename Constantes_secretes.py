3b362f1a2e0ff4bf0ebc68688668e954c0934934
ATField2501 <atfield2501@gmail.com>
2020-01-30 13:05:22 +0100

    boo

--- Constantes_secretes.py
+++ Constantes_secretes.py
@@ -4,8 +4,8 @@
 botname="Prague_Golem"
 server_irc_adresse="irc.epiknet.org"
 port=6667
-mdp_irc='U{"3l@X7'
+mdp_irc='xxxx'
 nickperso="Cagliostro"
 
 adresse_mail="atfield2501@gmail.com"
-mdp_mail="ioO072vrK5"
+mdp_mail="xxxx"
